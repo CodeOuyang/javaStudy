f2a115cc6ae021a13072620bf5e8d5a21fcae64e
ouyang <ouyang@ouyangdeiMac.local>
2020-02-07 20:11:01 +0800

    travel案例

--- Hello/src/cn/ouyang/hello.java
+++ Hello/src/cn/ouyang/hello.java
@@ -1,7 +1,8 @@
 package cn.ouyang;
 
 public class hello {
+    //psvm快捷键 sout
     public static void main(String[] args) {
-        System.out.println("ssss");
+        System.out.println("ss56你哈ss");
     }
 }
